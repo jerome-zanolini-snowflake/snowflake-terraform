b9f4d817eafeecc223d1764c71c5012bb479c120
ADM Jerome Zanolini <jerome.zanolini@chopard.ch>
2023-11-06 15:16:18 +0100

    Change schema

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "snowflake_database" "demo_db" {
 
 resource "snowflake_schema" "tf_demo_schema" {
   database = snowflake_database.demo_db.name
-  name     = "myschema3"
+  name     = "myschema4"
 }
 
 resource "snowflake_warehouse" "warehouse" {

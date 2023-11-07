48ca099e8b3ae23e20f892489a6db196625ea376
ADM Jerome Zanolini <jerome.zanolini@chopard.ch>
2023-11-07 14:42:16 +0100

    Change Provider

--- modules/snowflake_resources/db_schema.tf
+++ modules/snowflake_resources/db_schema.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.63.0"
+    }
+  }
+}
+
+
 resource "snowflake_database" "main_db" {
   name    = var.database
   comment = "Database for Snowflake Terraform demo 8"
